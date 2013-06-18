5a3c9eeb2c866ca190b1149aed87d7fc6865d68b
pgodzin <pgg2105@columbia.edu>
2013-06-17 23:16:02 -0300

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -71,22 +71,22 @@ $(document).ready(function(){
 "                                 ____     ",             
 "                               d88P'`88b    ",                
 "d8888888888888888F'           |888    88b      ",        
-       "      .dF'      __      `88b   d88P       __",  
-      "     .dF'     d888888b    `88b          d888888b",    
-     "    .dF'      d8b  `Y88b    `88b.       d8b  `Y88b  ",    
-    "   .dF'        888b(*)888xxxxxx888bxxxxxx888b(*)888     ",     
-   "  .dF'           888oo8P         `88b     888oo8P ", 
-  " .dF'               ~~~~     d88P   `88b       ~~~", 
- ".dF'                          88b.   888|             ",         
+"              .dF'      __      `88b   d88P       __",  
+"            .dF'     d888888b    `88b          d888888b",    
+"          .dF'      d8b  `Y88b    `88b.       d8b  `Y88b  ",    
+"        .dF'        888b(*)888xxxxxx888bxxxxxx888b(*)888     ",     
+"     .dF'           888oo8P         `88b     888oo8P ", 
+"   .dF'               ~~~~     d88P   `88b       ~~~", 
+" .dF'                          88b.   888|             ",         
 "d88b.                            88b.d88P      ", 
- "888b.                            ~~~~~           ",   
-    "888b.           .d8888b.      d888888888==-      ",                
-       "888b.        8888L 88b.    `888888P~~            __________          ", 
-          "888b.      8F  8888xxxxx8888xxxxxxxxxxx88888888888 ", 
-             "888b.        d8P~~~~~~", 
-                "888b.. ..d8F", 
-                   "888888F", 
-                      "~~~ "
+" 888b.                            ~~~~~           ",   
+"   888b.           .d8888b.      d888888888==-      ",                
+"      888b.        8888L 88b.    `888888P~~            __________          ", 
+"          888b.      8F  8888xxxxx8888xxxxxxxxxxx88888888888 ", 
+"             888b.        d8P~~~~~~", 
+"                888b.. ..d8F", 
+"                   888888F", 
+"                      ~~~ "
   ];
 
   var line = 0;
@@ -94,10 +94,10 @@ $(document).ready(function(){
   var sing = function () {
     console.log(song[line%song.length]);
     line++;
-    setTimeout(sing, 500);
+    setTimeout(sing, 1000);
   };
 
-  setTimeout(sing, 5000);
+  setTimeout(sing, 15000);
 
 });
 })(jQuery)
